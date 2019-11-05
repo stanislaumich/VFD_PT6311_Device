585009990682084d469a49946bec1fc262bf54ad
Stas <staslz42stas@gmail.com>
2019-11-05 16:24:40 +0300

    add

--- src/settings.h
+++ src/settings.h
@@ -4,11 +4,11 @@
 #define VFD_DATA_PIN D3 //brown 
 #define VFD_BYTES_PER_DIGIT 3 
 // numbers
-uint8_t f[10]={140,128,12,132,128,132,140,128,140,132};
-uint8_t s[10]={196,64,195,195,71,135,135,192,199,199};
+const PROGMEM uint8_t f[10]={140,128,12,132,128,132,140,128,140,132};
+const PROGMEM uint8_t s[10]={196,64,195,195,71,135,135,192,199,199};
 // symbols
-uint8_t sf[10]={140,128,12,132,128,132,140,128,140,132};
-uint8_t ss[10]={196,64,195,195,71,135,135,192,199,199};  
+const PROGMEM uint8_t sf[10]={140,128,12,132,128,132,140,128,140,132};
+const PROGMEM uint8_t ss[10]={196,64,195,195,71,135,135,192,199,199};  
 //
 unsigned long previousMillisMin;
 unsigned long previousMillisScr;
